804445d7f4151fbd91a10f95856e9b0ca246c34d
Stefan Pijnappels <stefan@puppet.com>
2016-08-18 14:52:54 +0100

    cleaned up lo interface handling

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -1,16 +1,16 @@
 class firewall_templates::proxy {
   firewall { '100 drop outgoing port 80':
-    proto	=> 'tcp',
-    chain	=> 'OUTPUT',
-    dport	=> '80',
-    destination	=> '! 192.168.0.0/16',
-    action	=> 'drop',
+    proto	      => 'tcp',
+    iniface     => '! lo',
+    chain	      => 'OUTPUT',
+    dport	      => '80',
+    action	    => 'drop',
   }
   firewall { '101 drop outgoing port 443':
     proto       => 'tcp',
+    iniface     => '! lo',
     chain       => 'OUTPUT',
     dport       => '443',
-    destination => '! 192.168.0.0/16',
     action      => 'drop',
   }
 }
